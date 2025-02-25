eda3eabbe2112926821f6591a8c51ac6adf8bd1a
jakimowb <benjamin.jakimow@geo.hu-berlin.de>
2025-02-25 04:37:05 +0100

    api_get lädt nun auch Pretalx JSONs

--- namensschilder/bin/api_get.py
+++ namensschilder/bin/api_get.py
@@ -3,49 +3,17 @@
 """
 Download der Pretix-Daten als JSON nach /namensschilder/data/<EventID>/*.json
 """
+import argparse
+import datetime
+from typing import Union
+
 import requests
 import json
 import os
 from pathlib import Path
 
-## CONFIG START
 
-# die Event ID in Pretix
-# https://pretix.eu/control/event/fossgis/<EventID>
-EVENT_ID = "2025"
-
-## CONFIG END
-
-# wir brauchen einen API-Token (und damit er geheim bleibt,
-# sollte er hier nicht im code stehen!)
-# siehe https://docs.pretix.eu/en/latest/api/auth.html
-if not 'PRETIX_API_TOKEN' in os.environ:
-    raise Exception("PRETIX_API_TOKEN variable nicht definiert.")
-
-# URL-endpoints
-BASEURL = "https://pretix.eu/api/v1/organizers/fossgis/events/"
-
-# https://docs.pretix.eu/en/latest/api/resources/orders.html#get--api-v1-organizers-(organizer)-events-(event)-orders-
-ORDER_URL = BASEURL + EVENT_ID + "/orders/"
-# https://docs.pretix.eu/en/latest/api/resources/invoices.html#get--api-v1-organizers-(organizer)-events-(event)-invoices-
-INVOICE_URL = BASEURL + EVENT_ID + "/invoices/"
-# ?????????????????
-NREI_URL = BASEURL + EVENT_ID + "/orders?identifier=dekodi_nrei"
-# https://docs.pretix.eu/en/latest/api/resources/items.html#get--api-v1-organizers-(organizer)-events-(event)-items-
-ITEMS_URL = BASEURL + EVENT_ID + "/items/"
-
-ITEM_CATEGORY_URL = BASEURL + EVENT_ID + "/categories/"
-
-QUESTIONS = BASEURL + EVENT_ID + "/questions/"
-
-
-# Auth*
-headers = {
-    "Authorization": f"Token {os.environ['PRETIX_API_TOKEN']}"
-}
-
-def getJsonData(url, filename):
-
+def getJsonData(url, filename, headers):
     results = []
 
     while url:
@@ -62,14 +30,113 @@ def getJsonData(url, filename):
     with open(filename, 'w') as file:
         json.dump(results, file, indent=4, ensure_ascii=False)
 
-# Call the functions to execute the code
 
-root = Path(__file__).parents[1] / "data" / EVENT_ID
-os.makedirs(root, exist_ok=True)
 
-getJsonData(QUESTIONS, root / "questions.json")
-getJsonData(ITEM_CATEGORY_URL, root / "categories.json")
-getJsonData(ORDER_URL, root / "orders.json")
-getJsonData(INVOICE_URL, root / "invoices.json")
-# getJsonData(NREI_URL, root / "nrei.json")
-getJsonData(ITEMS_URL, root / "items.json")
+def read_pretalx(event_id: str, token: str, dir_output: Union[str, Path]):
+
+    dir_output = Path(dir_output)
+    # URL-endpoints
+    BASEURL = f"https://pretalx.com/api/events/{event_id}"
+
+    # https://docs.pretix.eu/en/latest/api/resources/orders.html#get--api-v1-organizers-(organizer)-events-(event)-orders-
+    TALKS_URL = BASEURL + "/talks/"
+    ROOMS_URL = BASEURL + "/rooms/"
+
+    # Auth*
+    headers = {
+        "Authorization": f"Token {token}"
+    }
+
+
+    # Call the functions to execute the code
+    getJsonData(ROOMS_URL, dir_output / "pretalx_rooms.json", headers)
+    getJsonData(TALKS_URL, dir_output / "pretalx_talks.json", headers)
+
+
+def read_pretix(event_id: str, token: str, dir_output: Union[str, Path]):
+
+    dir_output = Path(dir_output)
+    # URL-endpoints
+    BASEURL = f"https://pretix.eu/api/v1/organizers/fossgis/events/{event_id}"
+
+    # https://docs.pretix.eu/en/latest/api/resources/orders.html#get--api-v1-organizers-(organizer)-events-(event)-orders-
+    ORDER_URL = BASEURL + "/orders/"
+    # https://docs.pretix.eu/en/latest/api/resources/invoices.html#get--api-v1-organizers-(organizer)-events-(event)-invoices-
+    INVOICE_URL = BASEURL + "/invoices/"
+    # ?????????????????
+    NREI_URL = BASEURL + "/orders?identifier=dekodi_nrei"
+    # https://docs.pretix.eu/en/latest/api/resources/items.html#get--api-v1-organizers-(organizer)-events-(event)-items-
+    ITEMS_URL = BASEURL + "/items/"
+
+    ITEM_CATEGORY_URL = BASEURL + "/categories/"
+
+    QUESTIONS = BASEURL + "/questions/"
+
+
+    # Auth*
+    headers = {
+        "Authorization": f"Token {token}"
+    }
+
+
+    # Call the functions to execute the code
+
+    getJsonData(QUESTIONS, dir_output / "pretix_questions.json", headers)
+    getJsonData(ITEM_CATEGORY_URL, dir_output / "pretix_categories.json", headers)
+    getJsonData(ORDER_URL, dir_output / "pretix_orders.json", headers)
+    getJsonData(ITEMS_URL, dir_output / "pretix_items.json", headers)
+    # getJsonData(INVOICE_URL, dir_output / "pretix_invoices.json", headers)
+    # getJsonData(NREI_URL, root / "pretix_nrei.json", headers)
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Download der Pretix- & Pretalx-Daten als JSON '
+                                                 'zur weiteren Verarbeitung')
+    parser.add_argument('-o', '--output',
+                        type=str,
+                        default=Path(__file__).parents[1] / "data" ,
+                        help='Ausgabeordner für die JSON-Dateien')
+    parser.add_argument('-y', '--year',
+                        type=int,
+                        default=datetime.date.today().year,
+                        help='Jahr der FOSSGIS')
+    parser.add_argument('--pretix',
+                        required=False,
+                        default=None,
+                        help='Pretix API Token (https://docs.pretix.eu/en/latest/api/auth.html)',
+                        action='store_true')
+    parser.add_argument('--pretalx',
+                        required=False,
+                        default=None,
+                        help='Pretalx API Token (https://docs.pretalx.org/api/fundamentals/#obtaining-an-api-token)',
+                        action='store_true')
+
+
+    args = parser.parse_args()
+
+    dir_output = Path(args.output)
+    os.makedirs(dir_output, exist_ok=True)
+
+    pretix_token = args.pretix
+    pretalx_token = args.pretalx
+    if pretix_token is None:
+        pretix_token = os.environ.get('PRETIX_TOKEN')
+
+    if pretalx_token is None:
+        pretalx_token = os.environ.get('PRETALX_TOKEN')
+
+    if pretix_token is None and pretalx_token is None:
+        raise ValueError('Es wurde weder ein Pretix- noch ein Pretalx-Token angegeben')
+
+    if pretalx_token:
+        event_id = f'fossgis{args.year}'
+        read_pretalx(event_id, pretalx_token, dir_output)
+
+    if pretix_token:
+        event_id = f'{args.year}'
+        read_pretix(event_id, pretalx_token, dir_output)
+
+
+
